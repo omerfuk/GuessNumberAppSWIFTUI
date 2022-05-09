9a61fc361ffbe3b4efa8f0364a27c51f29b60440
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-09 12:49:22 +0300

    Initial Commit

---
+++ GuessNumberAppSWIFTUI/GuessNumberAppSWIFTUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessNumberAppSWIFTUIApp.swift
+//  GuessNumberAppSWIFTUI
+//
+//  Created by Ömer Faruk Kılıçaslan on 9.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessNumberAppSWIFTUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
